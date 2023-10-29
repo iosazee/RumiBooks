918d3692490295d69ba51ec017589460e3609671
Zee Mudia <iosazee1@gmail.com>
2023-10-29 22:53:37 +0000

    created category views, urls and templates for crud ops

--- rumi_app/urls.py
+++ rumi_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home, privacy
+from .views import home, privacy, CategoryListView, CategoryCreateView, CategoryUpdateView, CustomCategoryDeleteView
 
 urlpatterns = [
     path('', home, name="home"),
     path('privacy/', privacy, name="privacy"),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/create', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/delete/<int:pk>/', CustomCategoryDeleteView.as_view(), name='category_delete'),
 ]
--- rumi_app/views.py
+++ rumi_app/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Category
+from .forms import CategoryForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.views import View
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -7,3 +13,71 @@ def home(request):
 
 def privacy(request):
     return render(request, 'privacy.html')
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/category_list.html'
+    context_object_name = 'categories'
+
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/category_form.html'
+    success_url = '/categories/'
+
+    def form_valid(self, form):
+        form.save()  # Save the form if it's valid
+        success_message = 'Category created successfully.'
+        messages.success(self.request, success_message)
+        return redirect(self.success_url)
+
+    def form_invalid(self, form):
+        error_message = 'Category creation failed. Please check the form for errors.'
+        messages.error(self.request, error_message)
+        return super().form_invalid(form)
+
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/category_form.html'
+    success_url = '/categories/'
+
+    def form_valid(self, form):
+        form.save()
+        success_message = 'Category updated successfully.'
+        messages.success(self.request, success_message)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        error_message = 'Category update failed. Please check the form for errors.'
+        messages.error(self.request, error_message)
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return self.success_url
+
+
+
+class CustomCategoryDeleteView(View):
+    model = Category
+    template_name = 'category/category_delete.html'
+    success_url = reverse_lazy('category_list')
+
+    def get(self, request, pk):
+        category = get_object_or_404(self.model, pk=pk)
+        return render(request, self.template_name, {'category': category})
+
+    def post(self, request, pk):
+        category = get_object_or_404(self.model, pk=pk)
+        category_name = category.name
+        category.delete()
+
+        success_message = f'Book "{category_name}" has been deleted.'
+        messages.success(request, success_message)
+
+        return redirect(self.success_url)
